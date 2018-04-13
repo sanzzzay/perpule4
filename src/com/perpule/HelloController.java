be5b7f50836b03e02f708de3d077559c40dbd2b0
Sanjay kr Singh <sanjay_singh@next-hyd-5877.nexteducation.com>
2018-04-13 08:46:02 +0530

    First commit

---
+++ src/com/perpule/HelloController.java
@@ -0,0 +1,24 @@
+package com.perpule;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("/hello")
+
+public class HelloController {
+	@GET
+	 @Produces(MediaType.APPLICATION_JSON)
+	 @Path("/customer/{customerId}")
+	 public Response getHelloWorld(@PathParam("customerId") String customerId)
+	{
+		
+		
+	  //return new HelloWorldObject();
+
+return Response.status(200).entity("Hello").build();
+	 }
+}
